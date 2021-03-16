1437f5cc13a1c818eede2afa90bca2a18a3b07b5
Jeffrey Wang <jw2175@gmail.com>
2021-03-16 16:47:29 -0700

    Add default finish implementation, deprecate finishedcallback

--- ios/ElectrodeApiImpl/APIImpls/Navigation/ENMiniAppNavDataProvider.swift
+++ ios/ElectrodeApiImpl/APIImpls/Navigation/ENMiniAppNavDataProvider.swift
@@ -16,14 +16,19 @@
 
 import Foundation
 
+@available(*, deprecated, message: "MiniAppFinishedCallback is deprecated, use Payload")
 public typealias MiniAppFinishedCallback = (String?) -> Void
+
 public typealias Payload = ([AnyHashable: Any]?) -> Void
 public typealias NavigateWithRoute = ([AnyHashable: Any]) -> Bool
 public typealias BackToRoute = (_ componentName: String, _ properties: [AnyHashable: Any]) -> Bool
 
 @objc public protocol ENMiniAppNavDataProvider: ENMiniAppDataProvider {
     // Ideally this protocol was named ENNavCallback, as it should be a call back handler
+
+    @available(*, deprecated, message: "finishedCallback is deprecated, use finish")
     @objc optional var finishedCallback: MiniAppFinishedCallback? { get set }
+
     @objc optional var finish: Payload? { get set }
     @objc optional var navigateWithRoute: NavigateWithRoute { get set }
     @objc optional var backToMiniApp: BackToRoute { get set }
